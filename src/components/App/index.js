fe65f0f0fa737ca97a92adfbed475ca78b75106b
Robbie Belson <rhbelson@gmail.com>
2019-01-09 21:26:28 -0600

    first draft of sketch implemented with all components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,14 +3,387 @@ import React, { Component } from 'react';
 // import Shelf from '../Shelf';
 // import FloatCart from '../FloatCart';
 
-class App extends Component {
+const PRODUCTS=[
+    {
+      "id": 12,
+      "sku": 12064273040195392,
+      "title": "Cat Tee Black T-Shirt",
+      "description": "4 MSL",
+      "availableSizes": ["S", "XS"],
+      "style": "Black with custom print",
+      "price": 10.9,
+      "installments": 9,
+      "currencyId": "USD",
+      "currencyFormat": "$",
+      "isFreeShipping": true
+    },
+
+    {
+      "id": 13,
+      "sku": 51498472915966366,
+      "title": "Dark Thug Blue-Navy T-Shirt",
+      "description": "",
+      "availableSizes": ["M"],
+      "style": "Front print and paisley print",
+      "price": 29.45,
+      "installments": 5,
+      "currencyId": "USD",
+      "currencyFormat": "$",
+      "isFreeShipping": true
+    },
+
+    {
+      "id": 14,
+      "sku": 10686354557628303,
+      "title": "Sphynx Tie Dye Wine T-Shirt",
+      "description": "GPX Poly 1",
+      "availableSizes": ["X", "L", "XL"],
+      "style": "Front tie dye print",
+      "price": 9.0,
+      "installments": 3,
+      "currencyId": "USD",
+      "currencyFormat": "$",
+      "isFreeShipping": true
+    },
+
+    {
+      "id": 15,
+      "sku": 11033926921508487,
+      "title": "Skuul",
+      "description": "Treino 2014",
+      "availableSizes": ["X", "L", "XL", "XXL"],
+      "style": "Black T-Shirt with front print",
+      "price": 14.0,
+      "installments": 5,
+      "currencyId": "USD",
+      "currencyFormat": "$",
+      "isFreeShipping": true
+    },
+
+    {
+      "id": 11,
+      "sku": 39876704341265606,
+      "title": "Wine Skul T-Shirt",
+      "description": "",
+      "availableSizes": ["X", "L"],
+      "style": "Wine",
+      "price": 13.25,
+      "installments": 3,
+      "currencyId": "USD",
+      "currencyFormat": "$",
+      "isFreeShipping": true
+    },
+
+    {
+      "id": 16,
+      "sku": 10412368723880253,
+      "title": "Short Sleeve T-Shirt",
+      "description": "",
+      "availableSizes": ["XS", "X", "L", "ML", "XL"],
+      "style": "Grey",
+      "price": 75.0,
+      "installments": 5,
+      "currencyId": "USD",
+      "currencyFormat": "$",
+      "isFreeShipping": true
+    },
+
+    {
+      "id": 0,
+      "sku": 8552515751438644,
+      "title": "Cat Tee Black T-Shirt",
+      "description": "14/15 s/nº",
+      "availableSizes": ["X", "L", "XL", "XXL"],
+      "style": "Branco com listras pretas",
+      "price": 10.9,
+      "installments": 9,
+      "currencyId": "USD",
+      "currencyFormat": "$",
+      "isFreeShipping": true
+    },
+
+    {
+      "id": 1,
+      "sku": 18644119330491312,
+      "title": "Sphynx Tie Dye Grey T-Shirt",
+      "description": "14/15 s/nº",
+      "availableSizes": ["X", "L", "XL", "XXL"],
+      "style": "Preta com listras brancas",
+      "price": 10.9,
+      "installments": 9,
+      "currencyId": "USD",
+      "currencyFormat": "$",
+      "isFreeShipping": true
+    },
+
+    {
+      "id": 2,
+      "sku": 11854078013954528,
+      "title": "Danger Knife Grey",
+      "description": "14/15 s/nº",
+      "availableSizes": ["X", "L"],
+      "style": "Branco com listras pretas",
+      "price": 14.9,
+      "installments": 7,
+      "currencyId": "USD",
+      "currencyFormat": "$",
+      "isFreeShipping": true
+    },
+
+    {
+      "id": 3,
+      "sku": 876661122392077,
+      "title": "White DGK Script Tee",
+      "description": "2014 s/nº",
+      "availableSizes": ["X", "L"],
+      "style": "Preto com listras brancas",
+      "price": 14.9,
+      "installments": 7,
+      "currencyId": "USD",
+      "currencyFormat": "$",
+      "isFreeShipping": true
+    },
+
+    {
+      "id": 4,
+      "sku": 9197907543445677,
+      "title": "Born On The Streets",
+      "description": "14/15 s/nº - Jogador",
+      "availableSizes": ["XL"],
+      "style": "Branco com listras pretas",
+      "price": 25.9,
+      "installments": 12,
+      "currencyId": "USD",
+      "currencyFormat": "$",
+      "isFreeShipping": false
+    },
+
+    {
+      "id": 5,
+      "sku": 10547961582846888,
+      "title": "Tso 3D Short Sleeve T-Shirt A",
+      "description": "14/15 + Camiseta 1º Mundial",
+      "availableSizes": ["X", "L", "XL"],
+      "style": "Preto",
+      "price": 10.9,
+      "installments": 9,
+      "currencyId": "USD",
+      "currencyFormat": "$",
+      "isFreeShipping": false
+    },
+
+    {
+      "id": 6,
+      "sku": 6090484789343891,
+      "title": "Man Tie Dye Cinza Grey T-Shirt",
+      "description": "Goleiro 13/14",
+      "availableSizes": ["XL", "XXL"],
+      "style": "Branco",
+      "price": 49.9,
+      "installments": 0,
+      "currencyId": "USD",
+      "currencyFormat": "$",
+      "isFreeShipping": true
+    },
+
+    {
+      "id": 7,
+      "sku": 18532669286405342,
+      "title": "Crazy Monkey Black T-Shirt",
+      "description": "1977 Infantil",
+      "availableSizes": ["S"],
+      "style": "Preto com listras brancas",
+      "price": 22.5,
+      "installments": 4,
+      "currencyId": "USD",
+      "currencyFormat": "$",
+      "isFreeShipping": true
+    },
+
+    {
+      "id": 8,
+      "sku": 5619496040738316,
+      "title": "Tso 3D Black T-Shirt",
+      "description": "",
+      "availableSizes": ["XL"],
+      "style": "Azul escuro",
+      "price": 18.7,
+      "installments": 4,
+      "currencyId": "USD",
+      "currencyFormat": "$",
+      "isFreeShipping": false
+    },
+
+    {
+      "id": 9,
+      "sku": 11600983276356165,
+      "title": "Crazy Monkey Grey",
+      "description": "",
+      "availableSizes": ["L", "XL"],
+      "style": "",
+      "price": 134.9,
+      "installments": 5,
+      "currencyId": "USD",
+      "currencyFormat": "$",
+      "isFreeShipping": true
+    },
+
+    {
+      "id": 10,
+      "sku": 27250082398145995,
+      "title": "On The Streets Black T-Shirt",
+      "description": "",
+      "availableSizes": ["L", "XL"],
+      "style": "",
+      "price": 49.0,
+      "installments": 9,
+      "currencyId": "USD",
+      "currencyFormat": "$",
+      "isFreeShipping": true
+    }
+  ];
+
+class ProductCategoryRow extends React.Component {
+  render() {
+    const category = this.props.category;
+    return (
+      <tr>
+        <th colSpan="2">
+          {category}
+        </th>
+      </tr>
+    );
+  }
+}
+
+class ProductRow extends React.Component {
+  render() {
+    const product = this.props.product;
+    const name = product.stocked ?
+      product.name :
+      <span style={{color: 'red'}}>
+        {product.name}
+      </span>;
+
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{product.price}</td>
+      </tr>
+    );
+  }
+}
+
+class ProductTable extends React.Component {
+  render() {
+    const filterText = this.props.filterText;
+    const inStockOnly = this.props.inStockOnly;
+
+    const rows = [];
+    let lastCategory = null;
+
+    this.props.products.forEach((product) => {
+      // if (product.name.indexOf(filterText) === -1) {
+      //   return;
+      // }
+      if (inStockOnly && !product.stocked) {
+        return;
+      }
+      if (product.category !== lastCategory) {
+        rows.push(
+          <ProductCategoryRow
+            category={product.category}
+            key={product.category} />
+        );
+      }
+      rows.push(
+        <ProductRow
+          product={product}
+          key={product.name}
+        />
+      );
+      lastCategory = product.category;
+    });
+
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+      </table>
+    );
+  }
+}
+
+class SearchBar extends React.Component {
+  render() {
+    const filterText = this.props.filterText;
+    const inStockOnly = this.props.inStockOnly;
+
+    return (
+      <form>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={filterText} />
+        <p>
+          <input
+            type="checkbox"
+            checked={inStockOnly} />
+          {' '}
+          Only show products in stock
+        </p>
+      </form>
+    );
+  }
+}
+
+class FilterableProductTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      filterText: '',
+      inStockOnly: false
+    };
+  }
+
   render() {
     return (
-      <div className="App">
-        <h1>Hi</h1>
+      <div>
+        <SearchBar
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+        />
+        <ProductTable
+          products={this.props.products}
+          filterText={this.state.filterText}
+          inStockOnly={this.state.inStockOnly}
+        />
       </div>
     );
   }
 }
+
+
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+        <FilterableProductTable products={PRODUCTS} />
+      </div>
+    );
+  }
+}
+
+
+
+
 
 export default App;
