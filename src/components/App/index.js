987fdb78888578d2c4bdb920a37e36ec03c628a8
Robbie Belson <rhbelson@gmail.com>
2019-01-09 18:46:11 -0600

    app component created

---
+++ src/components/App/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+// import Shelf from '../Shelf';
+// import FloatCart from '../FloatCart';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
